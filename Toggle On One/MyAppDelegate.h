265e4c1b2c2cfa0c99dca0298a0c8bc2bca1c4dd
Jordan Hudgens <jordan@wow.com>
2014-01-04 18:00:15 -0600

    Initial Commit

---
+++ Toggle On One/MyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MyAppDelegate.h
+//  Toggle On One
+//
+//  Created by Jordan Hudgens on 1/4/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Toggle On One/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  Toggle On One
+//
+//  Created by Jordan Hudgens on 1/4/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end

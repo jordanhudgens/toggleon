bb9e91ca0424e175695ca7146f7552f2fb7e5c27
Jordan Hudgens <jordan@wow.com>
2014-01-06 17:59:37 -0600

    first commit

--- Toggle On One/MyViewController.m
+++ Toggle On One/MyViewController.m
@@ -9,6 +9,12 @@
 #import "MyViewController.h"
 
 @interface MyViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label1;
+@property (weak, nonatomic) IBOutlet UILabel *label2;
+@property (weak, nonatomic) IBOutlet UILabel *label3;
+@property (weak, nonatomic) IBOutlet UISwitch *switchValue;
+
+@property (retain, nonatomic) IBOutletCollection(UILabel) NSArray *test;
 
 @end
 
@@ -18,6 +24,30 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)labelChange:(id)sender {
+    if (_switchValue.isOn) {
+        _label1.hidden;
+        _label2.hidden;
+        _label3.hidden;
+    } else {
+        _label1.text = @"my string";
+        _label2.text = @"my string2";
+        _label3.text = @"my string3";
+    }
+    NSArray *funnyWords = @[@"Schadenfreude", @"Portmanteau", @"Penultimate"];
+    
+    for (NSString *word in funnyWords) {
+        if (_switchValue.isOn) {
+            _label1.hidden;
+            _label2.hidden;
+            _label3.hidden;
+        } else {
+            _label1.text = @"my string";
+            _label2.text = @"my string2";
+            _label3.text = @"my string3";
+        }
+    }
 }
 
 - (void)didReceiveMemoryWarning

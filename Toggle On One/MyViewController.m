5f9d655aa7e42aec948f3fe159e0f7fc83d885f6
Jordan Hudgens <jordan@wow.com>
2014-01-06 20:36:28 -0600

    Created array for labels and built for loop to iterate

--- Toggle On One/MyViewController.m
+++ Toggle On One/MyViewController.m
@@ -4,17 +4,13 @@
 //
 //  Created by Jordan Hudgens on 1/4/14.
 //  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
-//  Testing Changes
+//
 
 #import "MyViewController.h"
 
 @interface MyViewController ()
-@property (weak, nonatomic) IBOutlet UILabel *label1;
-@property (weak, nonatomic) IBOutlet UILabel *label2;
-@property (weak, nonatomic) IBOutlet UILabel *label3;
 @property (weak, nonatomic) IBOutlet UISwitch *switchValue;
-
-@property (retain, nonatomic) IBOutletCollection(UILabel) NSArray *test;
+@property (retain, nonatomic) IBOutletCollection(UILabel) NSArray *arrayOfLabels;
 
 @end
 
@@ -26,26 +22,13 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 - (IBAction)labelChange:(id)sender {
-    if (_switchValue.isOn) {
-        _label1.hidden;
-        _label2.hidden;
-        _label3.hidden;
-    } else {
-        _label1.text = @"my string";
-        _label2.text = @"my string2";
-        _label3.text = @"my string3";
-    }
-    NSArray *funnyWords = @[@"Schadenfreude", @"Portmanteau", @"Penultimate"];
     
-    for (NSString *word in funnyWords) {
+    for (UILabel *label in self.arrayOfLabels)
+    {
         if (_switchValue.isOn) {
-            _label1.hidden;
-            _label2.hidden;
-            _label3.hidden;
+            [label setHidden:NO];
         } else {
-            _label1.text = @"my string";
-            _label2.text = @"my string2";
-            _label3.text = @"my string3";
+            [label setHidden:YES];
         }
     }
 }
